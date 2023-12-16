a5947579c264e1daec61d1a2dc77571c6eeea41c
Abijeet <63050833+abijeetraut1@users.noreply.github.com>
2023-12-16 20:47:41 +0545

    Update and rename Js conversations to Js_conversations.js

---
+++ task/Js_conversations.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+
+Indentation:
+Make your code look neat by using spaces (2 or 4) to separate different parts.Variable 
+
+Naming: 
+Give your variables names that make sense. Use camelCase, like myVariable.
+
+Semicolons: 
+Add semicolons at the end of your lines. It's like putting a full stop in a sentence.
+
+Comments: 
+Write notes in your code to explain tricky parts. Use // for short notes and /* */ for longer ones.
+
+Function Declaration: 
+When making functions, start with the word function, then give it a name and some info inside ().
+
+Console Logging: 
+To check what's happening in your code, use console.log(). It's like a diary for your code.
